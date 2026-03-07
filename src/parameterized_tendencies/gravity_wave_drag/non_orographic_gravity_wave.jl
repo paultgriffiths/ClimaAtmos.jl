838092b4816bb525bd8e79c5d488afe67fb3d853
ray-chew <chew@caltech.edu>
2026-03-06 17:53:44 -0800

    wip: Bug fix in NOGW

--- src/parameterized_tendencies/gravity_wave_drag/non_orographic_gravity_wave.jl
+++ src/parameterized_tendencies/gravity_wave_drag/non_orographic_gravity_wave.jl
@@ -49,6 +49,8 @@ function non_orographic_gravity_wave_cache(Y, gw::NonOrographicGravityWave)
             ᶜlevel,
             u_waveforcing = similar(Y.c.ρ),
             v_waveforcing = similar(Y.c.ρ),
+            u_waveforcing_top = similar(Fields.level(Y.c.ρ, 1)),
+            v_waveforcing_top = similar(Fields.level(Y.c.ρ, 1)),
             uforcing = similar(Y.c.ρ),
             vforcing = similar(Y.c.ρ),
             gw_ncval = Val(nc),
@@ -122,6 +124,8 @@ function non_orographic_gravity_wave_cache(Y, gw::NonOrographicGravityWave)
             ᶜlevel,
             u_waveforcing = similar(Y.c.ρ),
             v_waveforcing = similar(Y.c.ρ),
+            u_waveforcing_top = similar(Fields.level(Y.c.ρ, 1)),
+            v_waveforcing_top = similar(Fields.level(Y.c.ρ, 1)),
             uforcing = similar(Y.c.ρ),
             vforcing = similar(Y.c.ρ),
             gw_ncval = Val(nc),
@@ -451,7 +455,7 @@ function non_orographic_gravity_wave_forcing(
         )
 
         #extract the momentum flux outside the model top.
-        u_waveforcing_top = p.scratch.temp_field_level
+        u_waveforcing_top = p.non_orographic_gravity_wave.u_waveforcing_top
         copyto!(
             Fields.field_values(u_waveforcing_top),
             Fields.field_values(
@@ -461,14 +465,12 @@ function non_orographic_gravity_wave_forcing(
                 ),
             ),
         )
-        # u_waveforcing_top .=
-        #     Fields.level(u_waveforcing, Spaces.nlevels(axes(u_waveforcing)))
         fill!(
             Fields.level(u_waveforcing, Spaces.nlevels(axes(u_waveforcing))),
             0,
         )
 
-        v_waveforcing_top = p.scratch.temp_field_level_2
+        v_waveforcing_top = p.non_orographic_gravity_wave.v_waveforcing_top
         copyto!(
             Fields.field_values(v_waveforcing_top),
             Fields.field_values(
@@ -478,8 +480,6 @@ function non_orographic_gravity_wave_forcing(
                 ),
             ),
         )
-        # v_waveforcing_top .=
-        #     Fields.level(v_waveforcing, Spaces.nlevels(axes(v_waveforcing)))
         fill!(
             Fields.level(v_waveforcing, Spaces.nlevels(axes(v_waveforcing))),
             0,
